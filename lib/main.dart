f3cd9216345921988e52b00e053366ab3ab84e9e
AmmarAlharbii <ammaralharbi727@gmail.com>
2025-03-09 09:40:21 +0300

    feat:add splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:appointment_app/core/routing/app_router.dart';
 import 'package:appointment_app/doc_app.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
+  FlutterNativeSplash.remove();
 }
